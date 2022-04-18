5e4227c69208ea7a3d08d6e22b89ae3c9ff89844
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-18 13:55:49 +0300

    feature: update user class

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,11 @@
 export class User {
+  constructor(
+    public login: string,
+    public name: string,
+    public followers: string,
+    public membership: string,
+    public url: string,
+    public image: string,
+    public repos: number
+  ) {}
 }
