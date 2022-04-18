9f0e9f7320960c3927a1370dbcc7c8b38708e24d
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-18 10:26:25 +0300

    feature: add a routing feature to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserPopulatedComponent } from './components/user-populated/user-populated.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { UsersComponent } from './components/users/users.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserPopulatedComponent },
+  { path: 'repository', component: RepositoriesComponent },
+  { path: '', component: UsersComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
