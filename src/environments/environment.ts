18c558bdb270b945f315f75f5f73620e6cbd2a06
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-19 03:40:33 +0300

    feature: hide the enviroments.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   globalUserSearch: 'https://api.github.com/search/users?q=',
   userSearch: 'https://api.github.com/users/',
   globalRepoSearch: 'https://api.github.com/search/repositories?q=',
+  accessToken: 'jlkdsjflkjdslfjldsjfdslfhdsaglkjsa',
 };
 
 /*
