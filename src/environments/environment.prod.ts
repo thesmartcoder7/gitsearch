9eaf664abeed346f93338d0fef821c141167d295
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-19 08:30:15 +0300

    fix: bad requrest fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.github.com/',
+  globalUserSearch: 'https://api.github.com/search/users?q=',
+  userSearch: 'https://api.github.com/users/',
+  globalRepoSearch: 'https://api.github.com/search/repositories?q=',
+  accessToken: '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',
 };
