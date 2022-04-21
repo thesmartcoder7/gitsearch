a99275a16874412756a365a2ff623904786b3c19
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-21 04:44:25 +0300

    style: add the single repo view

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from 'src/app/services/api-service.service';
 import { Router } from '@angular/router';
+import { Repository } from 'src/app/classes/repository';
 
 @Component({
   selector: 'app-users',
@@ -12,6 +13,11 @@ export class UsersComponent implements OnInit {
   globalRepoReturn!: any[];
   isUsers: boolean = false;
   isRepositories: boolean = false;
+  isSingleRepository: boolean = false;
+
+  singleUserImage!: string;
+  singleUserLogin!: string;
+  singleRepository!: any;
 
   constructor(private apiCall: ApiServiceService, private route: Router) {}
 
@@ -23,11 +29,18 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  goToUserFromRepo(login: string) {
+    this.route.navigate(['/user'], {
+      queryParams: { data: login },
+    });
+  }
+
   globalRepositorySearch(userInput: string) {
     this.apiCall.globalRepositorySearch(userInput).then(
       (success) => {
         this.globalRepoReturn = this.apiCall.repositories;
         this.isRepositories = true;
+        this.isSingleRepository = false;
         this.isUsers = false;
       },
       (error) => {
@@ -43,11 +56,26 @@ export class UsersComponent implements OnInit {
         this.globalUserReturn = this.apiCall.users;
         this.isUsers = true;
         this.isRepositories = false;
+        this.isSingleRepository = false;
       },
       (error) => {
         alert('User not found');
         console.log(error);
       }
     );
+  }
+
+  showSingleRepo(i: number) {
+    this.singleRepository = this.globalRepoReturn[i];
+    console.log(this.singleRepository);
+    this.isSingleRepository = true;
+    this.isUsers = false;
+    this.isRepositories = false;
+  }
+
+  repoResults() {
+    this.isSingleRepository = false;
+    this.isUsers = false;
+    this.isRepositories = true;
   }
 }
