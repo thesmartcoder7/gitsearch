09d04b079e15e36c7c1ffe63038145cc3ce9f694
samuel martins <samuel.martins4.sm@gmail.com>
2022-04-25 23:10:48 +0300

    add environments folder to gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.github.com/',
+  globalUserSearch: 'https://api.github.com/search/users?q=',
+  userSearch: 'https://api.github.com/users/',
+  globalRepoSearch: 'https://api.github.com/search/repositories?q=',
+  accessToken: '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',
+};
