f2e83899f5cd12d2f7a37ea0cfa2a36b831936a2
samuel martins <samuel.martins4.sm@gmail.com>
2022-04-25 23:05:47 +0300

    fix: fix .gitignore cache

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   globalUserSearch: 'https://api.github.com/search/users?q=',
   userSearch: 'https://api.github.com/users/',
   globalRepoSearch: 'https://api.github.com/search/repositories?q=',
+  accessToken: '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',
 };
