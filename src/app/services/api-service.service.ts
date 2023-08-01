5b45c898a3069e695af28b593166461476633107
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-08-01 04:18:26 +0300

    chore: udpate number of items for search per search

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -11,7 +11,7 @@ export class ApiServiceService {
   users!: object[];
   userRepositories!: any[];
   repositories!: any[];
-  singleRepositoy!: object[]
+  singleRepositoy!: object[];
 
   constructor(private http: HttpClient) {}
 
@@ -25,11 +25,14 @@ export class ApiServiceService {
   globalUserSearch(userInput: string) {
     let promise = new Promise<void>((resolve, reject) => {
       this.http
-        .get<User[]>(`${environment.globalUserSearch}${userInput}`, {
-          headers: {
-            Authorization: `token ${environment.accessToken}`,
-          },
-        })
+        .get<User[]>(
+          `${environment.globalUserSearch}${userInput}&per_page=500`,
+          {
+            headers: {
+              Authorization: `token ${environment.accessToken}`,
+            },
+          }
+        )
         .subscribe({
           next: (res: any) => {
             this.users = res.items;
@@ -50,20 +53,20 @@ export class ApiServiceService {
   globalRepositorySearch(userInput: string) {
     let promise = new Promise<void>((resolve, reject) => {
       this.http
-        .get<any>(`${environment.globalRepoSearch}${userInput}`, {
+        .get<any>(`${environment.globalRepoSearch}${userInput}&per_page=500`, {
           headers: {
             Authorization: `token ${environment.accessToken}`,
           },
         })
         .subscribe({
           next: (res: any) => {
-            let tempStore = []
-            for(let item of res.items){
-              if(item.visibility == 'public'){
-                tempStore.push(item)
+            let tempStore = [];
+            for (let item of res.items) {
+              if (item.visibility == 'public') {
+                tempStore.push(item);
               }
             }
-            this.repositories = tempStore
+            this.repositories = tempStore;
             resolve();
           },
           error: (error: any) => {
@@ -106,11 +109,14 @@ export class ApiServiceService {
   userRepositoriesSearch(userInput: string) {
     let promise = new Promise<void>((resolve, reject) => {
       return this.http
-        .get<any[]>(`${environment.userSearch}${userInput}/repos?per_page=1000`, {
-          headers: {
-            Authorization: `token ${environment.accessToken}`,
-          },
-        })
+        .get<any[]>(
+          `${environment.userSearch}${userInput}/repos?per_page=1000`,
+          {
+            headers: {
+              Authorization: `token ${environment.accessToken}`,
+            },
+          }
+        )
         .subscribe({
           next: (res: any[]) => {
             this.userRepositories = res;
